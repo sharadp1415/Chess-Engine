d6035c2e925cbe106050bec30d06905e35fc1e31
Sharad Prasad <sharadp1415@gmail.com>
2022-03-16 16:24:46 -0400

    implemented bishop isValidMove w/o check for king

--- src/pieces/Bishop.java
+++ src/pieces/Bishop.java
@@ -9,16 +9,52 @@ public class Bishop extends Piece {
         super(isWhite);
     }
 
-    public boolean isValidMove(Square start, Square end, Board board) {
+    public boolean isValidMove(Square start, Square end, Board b) {
+        if (start.equals(end))
+            return false;
+
         if (Math.abs(start.xpos - end.xpos) != Math.abs(start.ypos - end.ypos)) {
+            System.out.println("bruh");
             return false;
         }
 
+        // check if any pieces are in between start and end square
+        if (start.xpos < end.xpos) {
+            if (start.ypos < end.ypos) {
+                // down and right
+                for (int i = 1; i < Math.abs(end.xpos - start.xpos); i++) {
+                    if (b.board[start.xpos + i][start.ypos + i].piece != null)
+                        return false;
+                }
+            } else {
+                // down and left
+                for (int i = 1; i < Math.abs(end.xpos - start.xpos); i++) {
+                    if (b.board[start.xpos + i][start.ypos - i].piece != null)
+                        return false;
+                }
+            }
+        } else {
+            if (start.ypos < end.ypos) {
+                // up and right
+                for (int i = 1; i < Math.abs(end.xpos - start.xpos); i++) {
+                    if (b.board[start.xpos - i][start.ypos + i].piece != null)
+                        return false;
+                }
+            } else {
+                // up and left
+                for (int i = 1; i < Math.abs(end.xpos - start.xpos); i++) {
+                    if (b.board[start.xpos - i][start.ypos - i].piece != null)
+                        return false;
+                }
+            }
+        }
+
+        // checks if end square has a same color piece
         if (end.piece != null && (end.piece.isWhite == start.piece.isWhite)) {
             return false;
         }
 
-        return false;
+        return true;
     }
 
     public String toString() {
