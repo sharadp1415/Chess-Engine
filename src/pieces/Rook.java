e4d8de04bacc6d5932263c489c1b112b9e15f56d
Sharad Prasad <sharadp1415@gmail.com>
2022-03-17 18:51:26 -0400

    implemented rook isValidMove w/o check for king

--- src/pieces/Rook.java
+++ src/pieces/Rook.java
@@ -9,7 +9,7 @@ public class Rook extends Piece {
         super(isWhite);
     }
 
-    public boolean isValidMove(Square start, Square end, Board board) {
+    public boolean isValidMove(Square start, Square end, Board b) {
         if (start.equals(end))
             return false;
 
@@ -18,15 +18,35 @@ public class Rook extends Piece {
             return false;
         }
 
+        // check if move goes through pieces
         if (start.rowpos - end.rowpos == 0) {
-
+            for (int i = 1; i < Math.abs(start.colpos - end.colpos); i++) {
+                if (start.colpos < end.colpos) {
+                    if (b.board[start.rowpos][start.colpos + i].piece != null)
+                        return false;
+                } else {
+                    if (b.board[start.rowpos][start.colpos - i].piece != null)
+                        return false;
+                }
+            }
+        } else {
+            for (int i = 1; i < Math.abs(start.rowpos - end.rowpos); i++) {
+                if (start.rowpos < end.rowpos) {
+                    if (b.board[start.rowpos + i][start.colpos].piece != null)
+                        return false;
+                } else {
+                    if (b.board[start.rowpos - i][start.colpos].piece != null)
+                        return false;
+                }
+            }
         }
 
+        // check if piece lands on same color piece
         if (end.piece != null && (end.piece.isWhite == start.piece.isWhite)) {
             return false;
         }
 
-        return false;
+        return true;
     }
 
     public String toString() {
