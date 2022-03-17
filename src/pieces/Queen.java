d2ff2111f574d5a7cdc2ebecb0595d0e7528905b
Sharad Prasad <sharadp1415@gmail.com>
2022-03-17 18:55:58 -0400

    implemented queen isValidMove w/o check for king

--- src/pieces/Queen.java
+++ src/pieces/Queen.java
@@ -8,10 +8,69 @@ public class Queen extends Piece {
         super(isWhite);
     }
 
-    public boolean isValidMove(Square start, Square end, Board board) {
+    public boolean isValidMove(Square start, Square end, Board b) {
+        if (start.equals(end))
+            return false;
+
+        // check for valid move direction
         if (Math.abs(start.rowpos - end.rowpos) != Math.abs(start.colpos - end.colpos)
                 && (start.rowpos - end.rowpos) * (start.colpos - end.colpos) != 0) {
             return false;
+        }
+
+        // check if any pieces are between start and end square
+        if (start.rowpos - end.rowpos == 0 || start.colpos - end.colpos == 0) {
+            if (start.rowpos - end.rowpos == 0) {
+                for (int i = 1; i < Math.abs(start.colpos - end.colpos); i++) {
+                    if (start.colpos < end.colpos) {
+                        if (b.board[start.rowpos][start.colpos + i].piece != null)
+                            return false;
+                    } else {
+                        if (b.board[start.rowpos][start.colpos - i].piece != null)
+                            return false;
+                    }
+                }
+            } else {
+                for (int i = 1; i < Math.abs(start.rowpos - end.rowpos); i++) {
+                    if (start.rowpos < end.rowpos) {
+                        if (b.board[start.rowpos + i][start.colpos].piece != null)
+                            return false;
+                    } else {
+                        if (b.board[start.rowpos - i][start.colpos].piece != null)
+                            return false;
+                    }
+                }
+            }
+        } else {
+            if (start.rowpos < end.rowpos) {
+                if (start.colpos < end.colpos) {
+                    // down and right
+                    for (int i = 1; i < Math.abs(end.rowpos - start.rowpos); i++) {
+                        if (b.board[start.rowpos + i][start.colpos + i].piece != null)
+                            return false;
+                    }
+                } else {
+                    // down and left
+                    for (int i = 1; i < Math.abs(end.rowpos - start.rowpos); i++) {
+                        if (b.board[start.rowpos + i][start.colpos - i].piece != null)
+                            return false;
+                    }
+                }
+            } else {
+                if (start.colpos < end.colpos) {
+                    // up and right
+                    for (int i = 1; i < Math.abs(end.rowpos - start.rowpos); i++) {
+                        if (b.board[start.rowpos - i][start.colpos + i].piece != null)
+                            return false;
+                    }
+                } else {
+                    // up and left
+                    for (int i = 1; i < Math.abs(end.rowpos - start.rowpos); i++) {
+                        if (b.board[start.rowpos - i][start.colpos - i].piece != null)
+                            return false;
+                    }
+                }
+            }
         }
 
         if (end.piece != null && (end.piece.isWhite == start.piece.isWhite)) {
