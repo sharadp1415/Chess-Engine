5402205daf769d38b132c01d18999defd8c9375e
Sharad Prasad <sharadp1415@gmail.com>
2022-05-10 19:45:31 -0400

    created engine package and class

---
+++ src/engine/Engine.java
@@ -0,0 +1,15 @@
+package engine;
+
+import chess.Chess;
+
+public class Engine {
+
+    public static int evaluatePosition(Chess game) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
